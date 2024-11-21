c75fc2dbf49e8a43049ec59697998bc56544fa5d
dazzlemoepy <100792969+dazzlemoepy@users.noreply.github.com>
2024-11-21 10:31:28 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_OLpWXcaUO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6vjbuuhsv3l9a5nv71vib632m2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
